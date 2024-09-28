6a4ed29b387916fd3659f01c8ea7709f4804fd21
cryskram <vageeshgn2005@gmail.com>
2024-09-28 10:20:17 +0530

    added flutter safe-directory

--- programs/git.nix
+++ programs/git.nix
@@ -7,6 +7,7 @@
     userEmail = "vageeshgn2005@gmail.com";
     extraConfig = {
       init.defaultBranch = "main";
+      safe.directory = "/nix/store/vdlr4bw9p16xcd32gzx9pamfmyvcgiqw-flutter-wrapped-3.22.0-sdk-links";
     };
   };
 }
